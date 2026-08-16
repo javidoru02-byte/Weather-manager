6e8aad4686bb9dfa968b890d497bed62d0862324
ynkrpnk <ankarpenko35@gmail.com>
2026-08-16 16:38:23 +0300

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 
-function App() {}
+function App() {
+    return(
+        <div className="app">
+            <Footer />
+        </div>
+    )
+}
 
 export default App;
