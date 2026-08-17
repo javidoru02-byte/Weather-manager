80df0ac661b7bb4928db998abea322a4670bd28e
David <javidoru02@gmail.com>
2026-08-18 01:22:20 +0300

    add weatherSlice

---
+++ src/store/slices/weatherSlice.js
@@ -0,0 +1,78 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { apiForToday, forecastApi, apiKey } from "../../constant/WeatherApi";
+
+const initialState = {
+  weatherData: null,
+  forecastData: null,
+  loading: false,
+  error: null,
+};
+
+export const getWeather = createAsyncThunk(
+  "weather/getWeather",
+
+  async function (city, { rejectWithValue }) {
+    try {
+      const forToday = await axios.get(
+        `${apiForToday}?q=${city}&appid=${apiKey}&units=metric`,
+      );
+
+      return forToday.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  },
+);
+
+export const getWeatherForFiveDays = createAsyncThunk(
+  "weather/getWeatherForFiveDays",
+
+  async function (city, { rejectWithValue }) {
+    try {
+      const forFiveDays = await axios.get(
+        `${forecastApi}?q=${city}&appid=${apiKey}&units=metric`,
+      );
+
+      return forFiveDays.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  },
+);
+
+const setError = (state, action) => {
+  state.error = action.payload;
+  state.loading = false;
+};
+
+const setLoading = (state) => {
+  state.loading = true;
+  state.error = null;
+};
+const weatherSlice = createSlice({
+  name: `weather`,
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getWeather.fulfilled, (state, { payload }) => {
+      state.weatherData = payload;
+      state.loading = false;
+      state.error = null;
+    });
+
+    builder.addCase(getWeather.rejected, setError);
+    builder.addCase(getWeather.pending, setLoading);
+
+    builder.addCase(getWeatherForFiveDays.fulfilled, (state, { payload }) => {
+      state.forecastData = payload;
+      state.loading = false;
+      state.error = null;
+    });
+
+    builder.addCase(getWeatherForFiveDays.rejected, setError);
+    builder.addCase(getWeatherForFiveDays.pending, setLoading);
+  },
+});
+
+export default weatherSlice.reducer;
