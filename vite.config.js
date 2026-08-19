1a1d6f3eb54808e075957997469f41cff7205b7a
Turcew <K00fillia@gmail.com>
2026-08-19 15:01:04 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import Inspect from "vite-plugin-inspect";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), Inspect()],
+  server: { port: 3000, open: true },
+});
