19dc6f945e80a74ea55121d43218d378778c8071
ynkrpnk <ankarpenko35@gmail.com>
2026-08-20 19:53:40 +0300

    Header using Material UI (logo, burger menu, and account)

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
 import Header from './components/Header/Header/Header'
 import MainLayout from './components/MainLauout/MainLayout/MainLayout';
 import Footer from './components/Footer/Footer';
 
 import "./App.css";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#7d75ee',
+      light: '#D6C8E1',
+      dark: '#82A2B5'
+    }
+  }
+});
+
 function App() {
     return(
-        <div className="app">
-            <Header />
-            <MainLayout />
-            <Footer />
-        </div>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <div className="app">
+                <Header />
+                <MainLayout />
+                <Footer />
+            </div>
+        </ThemeProvider>
     )
 }
 
